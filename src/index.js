6f82073161f59cb25b753d8ae5aaa46d2992246f
PavelSolovev <pashius2010@gmail.com>
2020-09-01 00:47:00 +0400

    parameter actions

--- src/index.js
+++ src/index.js
@@ -8,6 +8,9 @@ const reducer = (state = 0, action) => {
     case "DEC":
       return state - 1;
 
+    case "RND":
+      return state + action.payload;
+
     default:
       return state;
   }
@@ -16,15 +19,23 @@ const reducer = (state = 0, action) => {
 const store = createStore(reducer);
 
 document.getElementById("inc").addEventListener("click", () => {
-  store.dispatch({type: "INC"})
+  store.dispatch({ type: "INC" });
 });
 
 document.getElementById("dec").addEventListener("click", () => {
-  store.dispatch({type: "DEC"})
+  store.dispatch({ type: "DEC" });
+});
+
+document.getElementById("rnd").addEventListener("click", () => {
+  const payload = Math.floor(Math.random() * 10);
+  store.dispatch({
+    type: "RND",
+    payload,
+  });
 });
 
 const update = () => {
   document.getElementById("counter").innerHTML = store.getState();
-}
+};
 
 store.subscribe(update);
